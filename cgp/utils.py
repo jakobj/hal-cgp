cc714429b915cc086965b5557c1a189cb584f22b
Jakob Jordan <jakobjordan@posteo.de>
2021-02-01 12:35:29 +0100

    Fix type in disk cache

--- cgp/utils.py
+++ cgp/utils.py
@@ -235,7 +235,7 @@ def disk_cache(
         __check_cache_consistency(fn, func)
 
         @functools.wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Union[float, None]:
+        def wrapper(*args: Any, **kwargs: Any) -> float:
 
             key: str = compute_key(*args, **kwargs)
 
